e92e7e80ebe74d91daeac17d32eeae6dac79fc38
Blackmamba <122316561+FantasticSukhi@users.noreply.github.com>
2025-01-01 15:10:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,24 +3,22 @@ from os import getenv
 from dotenv import load_dotenv
 
 if os.path.exists("local.env"):
-    load_dotenv("local.env")
+    load_dotenv("local.env")  # Correct indentation
 
+API_ID = int(getenv("API_ID", "6435225"))  # Optional
+API_HASH = getenv("API_HASH", "")  # Optional
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
-
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID"))
-MONGO_URL = getenv("MONGO_URL")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split())) if getenv("SUDO_USERS") else []
+OWNER_ID = int(getenv("OWNER_ID", 0))
+MONGO_URL = getenv("MONGO_URL", None)
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/3c52a01057865f7511168.jpg')
-ALIVE_TEXT = getenv("ALIVE_TEXT")
-PM_LOGGER = getenv("PM_LOGGER")
-LOG_GROUP = getenv("LOG_GROUP")
-GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
-BRANCH = getenv("BRANCH", "master") #don't change
- 
+ALIVE_TEXT = getenv("ALIVE_TEXT", "I'm alive!")
+PM_LOGGER = getenv("PM_LOGGER", None)
+LOG_GROUP = getenv("LOG_GROUP", None)
+GIT_TOKEN = getenv("GIT_TOKEN", None)  # Personal access token
+REPO_URL = getenv("REPO_URL", "https://github.com/FantasticSukhi/MBV-UB")
+BRANCH = getenv("BRANCH", "master")  # Don't change
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
